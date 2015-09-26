df7ff314b6d4bbc7b65ba3d2d47e4e5e5a5a66cb
Mason Davenport <masondaven@gmail.com>
2015-09-25 23:43:34 -0400

    added DashboardViewController

---
+++ Thinger/DashboardViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DashboardViewController.swift
+//  Thinger
+//
+//  Created by Mason Davenport on 9/25/15.
+//  Copyright (c) 2015 Mason Davenport. All rights reserved.
+//
+
+import Foundation
